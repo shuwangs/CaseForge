5dc5a48a9d3b233aa82e348597fc5305c718c8c7
Shu Wang <swang3130@gatech.edu>
2026-04-25 21:47:06 -0400

    test: add publication.helper test and frontend publicationAPI test

---
+++ client/src/apis/publicationAPI.test.ts
@@ -0,0 +1,30 @@
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { fetchPublications, postPublications } from "./publicationAPI.ts";
+
+describe("publicationApi", async () => {
+    it("fetchPublications sends a POST request with ORCID ID", async () => {
+        const mockResponse = {
+            success: true,
+            data: [{ title: "Example publication" }],
+        }
+
+        const fetchMock = vi.spyOn(globalThis, "fetch").mockResolvedValue({
+            ok: true,
+            json: async () => mockResponse,
+        } as Response)
+
+        const result = await fetchPublications("0000-0002-2164-6551");
+        expect(fetchMock).toHaveBeenCalledWith("api/publication/search", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                orcid: "0000-0002-2164-6551",
+            })
+        })
+
+        expect(result).toEqual(mockResponse.data);
+    })
+
+})
---
+++ server/utitls/publication.helper.test.js
@@ -0,0 +1,25 @@
+import { describe, expect, it } from 'vitest';
+
+import {
+    extractIds,
+    parseJournal,
+    parsePublisher,
+    normalizePublication,
+} from "./publication.helper.js";
+
+describe("publication.helper test", () => {
+    describe("extractIds", () => {
+        it("extracts doi, openalexId, and pmid from the id strings", () => {
+            const idString = "doi:10.1002/ps.7676 openalex:W4385074293 omid:br/06290184524"
+
+            const result = extractIds(idString);
+            expect(result).toEqual({
+                doi: "10.1002/ps.7676",
+                openalexId: "W4385074293",
+                pmid: null
+            })
+
+
+        })
+    })
+})
