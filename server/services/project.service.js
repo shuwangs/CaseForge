9fa2800793ce77c38836067b639b96f0ca4a8bc9
Shu Wang <swang3130@gatech.edu>
2026-04-29 21:24:10 -0400

    chore: reformat

--- server/services/project.service.js
+++ server/services/project.service.js
@@ -1,15 +1,15 @@
 import pool from "../db/db.js";
 
 export const getProjectsByUserId = async (userId) => {
-    const result = await pool.query(
-        `
+	const result = await pool.query(
+		`
             SELECT *
             FROM caseforge.projects
             WHERE user_id = $1
             ORDER BY created_at DESC
         `,
-        [userId],
-    );
+		[userId],
+	);
 
-    return result.rows;
+	return result.rows;
 };
