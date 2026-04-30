3b90dc1c1d7c2947e1983eae214ce6113dae2c0a
Shu Wang <swang3130@gatech.edu>
2026-04-30 02:37:45 -0400

    feat(backend): add project creation and persist to database
    - implement addProject service
    - insert project into caseforge.projects

--- server/controllers/project.controller.js
+++ server/controllers/project.controller.js
@@ -1,5 +1,8 @@
 import AppError from "../errors/AppError.js";
-import { getProjectsByUserId } from "../services/project.service.js";
+import {
+	addProject,
+	getProjectsByUserId,
+} from "../services/project.service.js";
 import { userIdValidate } from "../utitls/userIdValidate.js";
 export const getProjects = async (req, res, next) => {
 	try {
@@ -12,6 +15,25 @@ export const getProjects = async (req, res, next) => {
 		const result = await getProjectsByUserId(userId);
 
 		res.status(200).json({
+			success: true,
+			data: result,
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const createProject = async (req, res, next) => {
+	try {
+		const project = req.body;
+		console.log("in controller the passed in project is ", project);
+		if (!userIdValidate(project.userId)) {
+			throw new AppError("Invalid userId", 400);
+		}
+
+		const result = await addProject(project);
+
+		res.status(201).json({
 			success: true,
 			data: result,
 		});
--- server/routes/project.publication.route.js
+++ server/routes/project.publication.route.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getProjects } from "../controllers/project.controller.js";
+import {
+	getProjects,
+	createProject,
+} from "../controllers/project.controller.js";
 import { savePublications } from "../controllers/project.publication.controller.js";
 
 const router = Router();
 
 router.post("/:projectId/publications", savePublications);
 router.get("/user/:userId", getProjects);
+router.post("/", createProject);
 
 export default router;
