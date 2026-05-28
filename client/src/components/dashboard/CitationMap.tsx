becaeaf1df9020bf49b3d66bbd48914037060142
Shu Wang <swang3130@gatech.edu>
2026-05-27 21:16:42 -0400

    refactor: move downloadMap to a helper file

--- client/src/components/dashboard/CitationMap.tsx
+++ client/src/components/dashboard/CitationMap.tsx
@@ -5,7 +5,7 @@ import useCitation from "../../contexts/useCitation.js";
 import useSummary from "../../contexts/useSummary.js";
 import AICard from "../ui/AICard.js";
 import BaseBtn from "../ui/BaseBtn.js";
-
+import { downloadMap } from "../../utils/downloadMap.js";
 const CitationMap = () => {
 	const mapRef = useRef(null);
 	const { projectId } = useParams();
@@ -13,41 +13,6 @@ const CitationMap = () => {
 	const { mapSummary, handleGenerateMapSummary } = useSummary();
 
 	const mapData = citationMap ?? [];
-
-	const downloadMap = () => {
-		const svgMap = mapRef.current?.querySelector("svg");
-		if (!svgMap) return;
-
-		const svgString = new XMLSerializer().serializeToString(svgMap);
-		const svgDataBase64 = btoa(unescape(encodeURIComponent(svgString)));
-		const svgDataUrl = `data:image/svg+xml;charset=utf-8;base64,${svgDataBase64}`;
-
-		const image = new Image();
-
-		image.onload = () => {
-			const canvas = document.createElement("canvas");
-			const width = 1000;
-			const height = 600;
-
-			canvas.width = width;
-			canvas.height = height;
-
-			const context = canvas.getContext("2d");
-			if (!context) return;
-
-			context.fillStyle = "white";
-			context.fillRect(0, 0, width, height);
-			context.drawImage(image, 0, 0, width, height);
-
-			const pngUrl = canvas.toDataURL("image/png");
-
-			const link = document.createElement("a");
-			link.href = pngUrl;
-			link.download = `caseforge-citation-map-${Date.now()}.png`;
-			link.click();
-		};
-		image.src = svgDataUrl;
-	};
 
 	if (loading) {
 		return <p>Loading citation map...</p>;
@@ -77,7 +42,9 @@ const CitationMap = () => {
 			</div>
 
 			<div>
-				<BaseBtn variant="secondary" onClick={downloadMap}>
+				<BaseBtn variant="secondary" onClick={
+					() => downloadMap(mapRef.current, `caseforge-citation-map-${Date.now()}.png`)
+				}>
 					Download Map
 				</BaseBtn>
 				<BaseBtn
