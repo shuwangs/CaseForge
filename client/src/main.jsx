755bdc024f72dad488ac36557188ff77bb0c4d57
Shu Wang <swang3130@gatech.edu>
2026-06-05 13:34:38 -0400

    update the colors:

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -23,15 +23,16 @@ const ClerkWithRouter = () => {
 			signUpForceRedirectUrl="/projects"
 			signInForceRedirectUrl="/projects"
 			afterSignOutUrl="/"
+
 			appearance={{
 				variables: {
 					colorPrimaryForeground: "#025f67", // used for text on the primary background.
-					colorBackground: "#2f9d94", //The background color for the card container
-					colorMuted: "#2f9d94", //used for muted backgrounds
-					colorInputForeground: "#025f67", //The color used for text in input fields.
-					colorInput: "#f7f6f2", //The background color used for input fields.
+					colorBackground: "#2f9d94",   //The background color for the card container
+					colorMuted: "#2f9d94",  //used for muted backgrounds
+					colorInputForeground: "#025f67",  //The color used for text in input fields.
+					colorInput: "#f7f6f2",   //The background color used for input fields.
 					colorPrimary: "#f7f6f2",
-					colorShimmer: "#f7f6f2", // color of the avatar shimmer.
+					colorShimmer: "#f7f6f2",  // color of the avatar shimmer.
 					colorRing: "#025f67", //lor of the ring when an interactive element is focused.
 					fontFamily: '"IBM Plex Sans", system-ui, sans-serif',
 
